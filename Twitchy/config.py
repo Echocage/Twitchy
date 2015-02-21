ad427448c0f234fb9af61a3cbf4346f0fbeb5f1f
Echocage <Echocage@users.noreply.github.com>
2015-02-21 05:53:51 -0800

    Update config.py

--- Twitchy/config.py
+++ Twitchy/config.py
@@ -5,8 +5,8 @@ config = {  # details required to login to twitch IRC server
             'port': 6667,
             'username': 'twitch_username',
             'oauth_password': 'oauth:',  # get this from http://twitchapps.com/tmi/  # channel to join
-            'channels': ['#channel_one', '#channel_two'],  # if set to true will display any data received
-            'debug': False,
-            'log_messages': True,  # maximum amount of bytes to receive from socket - 1024-4096 recommended
-            'socket_buffer_size': 2048
+            'channels': ['#channel_one', '#channel_two'],  
+            'debug': False, # if set to true will display any data received
+            'log_messages': True,  
+            'socket_buffer_size': 2048# maximum amount of bytes to receive from socket - 1024-4096 recommended
 }
