8f5844309d99242684ebc44195687e16ab8de9e6
Echocage <preston3050@gmail.com>
2015-02-21 04:50:44 -0800

    Removed old config values, simplified example

--- Twitch/config.py
+++ Twitch/config.py
@@ -7,27 +7,6 @@ config = {  # details required to login to twitch IRC server
             'oauth_password': 'oauth:',  # get this from http://twitchapps.com/tmi/  # channel to join
             'channels': ['#channel_one', '#channel_two'],  # if set to true will display any data received
             'debug': False,
-
-
-            'cron': {
-                '#channel_one': {
-                    'run_cron': False,  # False = No cron but still saves messages
-                    'run_time': 5,  # time in seconds
-                    'cron_messages': [
-                        'This is channel_one cron message one.',
-                        'This is channel_one cron message two.'
-                    ]
-                },
-
-                '#channel_two': {
-                    'run_cron': False,
-                    'run_time': 20,
-                    'cron_messages': [
-                        'This is channel_two cron message one.'
-                    ]
-                }
-            },  # if set to true will display any data received
-            'debug': False,  # if set to true will log all messages from all channels  # todo
             'log_messages': True,  # maximum amount of bytes to receive from socket - 1024-4096 recommended
             'socket_buffer_size': 2048
 }
--- example.py
+++ example.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
-import logging
-
 from Twitch import Irc
 from Twitch.config import config
 
-logging.basicConfig(level=logging.DEBUG)
 
 twitch_chat = Irc.from_config(config)
 twitch_chat.login()
